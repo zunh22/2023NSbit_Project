1ef47303e8c20794840889e5b463a19c03cbc60a
zunh22 <dhwnsgml13@naver.com>
2023-11-08 19:33:08 +0900

    i send you

--- project2/app.js
+++ project2/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7992ee7d94bbad1cd5e138e2f4ac8eea2dd18493c769d5cb1bbfe8f7494c69e5
-size 2389
+oid sha256:c647cac6d61fa5490c913bb5fc2a30cb32f7da67241e121dc5e3ed3eb1dca96a
+size 2396
