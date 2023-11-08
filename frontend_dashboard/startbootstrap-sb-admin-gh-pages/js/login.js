f0ade7e8fa3d4ecb13111f93eba6fcee9f3fd15e
Donguk03 <22011778@sju.ac.kr>
2023-11-08 19:32:21 +0900

    한마디

--- frontend_dashboard/startbootstrap-sb-admin-gh-pages/js/login.js
+++
@@ -1,7 +0,0 @@
-function login() {
-  var email=document.querySelector('#inputEmail');
-  var pw=document.querySelector('#inputPassword');
-
-  if(email.value=="admin"&&pw.value=="admin") location.href='./index.html';
-  else alert("로그인 실패");
-}
--- project2/app.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7992ee7d94bbad1cd5e138e2f4ac8eea2dd18493c769d5cb1bbfe8f7494c69e5
-size 2389
