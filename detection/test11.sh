a227f12550e2e2149cf1d02d9cd2100bec0afeef
say <ayoung07275@gmail.com>
2023-11-09 23:58:59 +0900

    detection

--- detection/test11.sh
+++ detection/test11.sh
@@ -4,22 +4,20 @@ container_name="$1"
 
 date
 
-timeout 60 docker exec "$container_name" sudo tcpdump -i eth0 -nn -e -tttt -X >> /home/hyeyeon/detection/tcpdump_$container_name.txt
+timeout 60 docker exec "$container_name" tcpdump -i eth0 -nn -e -tttt -X >> /home/say/2023NSbit_Project/packet/tcpdump_$container_name.txt
 
-whitelist_user="root"
-whitelist_pw="gpdus0405"
-whitelist_db="project"
+db_user="root"
+db_pw="1234pkj"
+db_name="project"
 whitelist_table="whitelist"
-
-rule_user="root"
-rule_pw="gpdus0405"
-rule_db="project"
 rule_table="rule"
 
 while IFS= read -r line; do
 	syn_detected=false
 	arp_detected=false
 
-	protocol=$(mysql -u $rule_user -p"$rule_pw" -D $rule_db -se "SELECT protocol FROM $rule_table")
+	protocol=$(mysql -u $db_user -p"$db_pw" -D $db_name -se "SELECT protocol FROM $rule_table")
 
-done < /home/hyeyeon/detection/tcpdump_$container_name.txt
+	
+
+done < /home/say/2023NSbit_Project/packet/tcpdump_$container_name.txt
