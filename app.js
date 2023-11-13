530ca8dcebac15b8e8fc29cdc88d4cdc01ea7153
Donguk03 <22011778@sju.ac.kr>
2023-11-14 01:30:34 +0900

    app.js console.log 구문 넣어서 디버깅 시도

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:047950b0f1bc051906bf42a422bf090c88fea359c85591050efea24cdbd39ffb
-size 4434
+oid sha256:e4abf9f8bcfaf95c562244bf55d0ae67cb13e31b7327bc701033bdf47ac2a022
+size 4244
