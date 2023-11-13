fa1d5893f2e5d18717e8835c813b7b9c9c6c2fa8
say <ayoung07275@gmail.com>
2023-11-13 19:08:03 +0900

    packet

--- detection/test7.sh
+++ detection/test7.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while true; do
-    #container_names=($(docker ps --format "{{.Names}}"))
-    container_names="attack_phpmyadmin_1"
+    container_names=($(docker ps --format "{{.Names}}"))
+    #container_names="attack_phpmyadmin_1"
     for container_name in "${container_names[@]}"; do
         ./test11.sh "$container_name" &
     done
