7bfbe5acb6d535c757155c21c2f39de751e5c888
bsh8798 <beak8798@naver.com>
2023-11-13 18:14:04 +0900

    ip주소

--- frontend_dashboard/startbootstrap-sb-admin-gh-pages/js/readPacket(x).js
+++ frontend_dashboard/startbootstrap-sb-admin-gh-pages/js/readPacket(x).js
@@ -14,30 +14,26 @@ xhr.onreadystatechange = function () {
             if (i + 1 < lines.length) {
                 const line1 = lines[i].trim();
                 const line2 = lines[i + 1].trim();
-
-                // 정규식을 사용하여 정보 추출
-                const timestampMatch = line1.match(/(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+)/);
-                const timestamp = timestampMatch ? timestampMatch[1] : null;
-
-                const sourceIpMatch = line2.match(/(\d+\.\d+\.\d+\.\d+)\.(\d+)/);
-                const sourceIp = sourceIpMatch ? sourceIpMatch[1] : null;
-                const sourcePort = sourceIpMatch ? sourceIpMatch[2] : null;
-
-                const destinationIpMatch = line2.match(/(\d+\.\d+\.\d+\.\d+)\.(\d+)/);
-                const destinationIp = destinationIpMatch ? destinationIpMatch[1] : null;
-                const destinationPort = destinationIpMatch ? destinationIpMatch[2] : null;
-
-                const flagsMatch = line2.match(/Flags \[([^\]]+)\]/);
-                const flags = flagsMatch ? flagsMatch[1] : null;
-
-                const protocolTypeMatch = line2.match(/proto (\w+) \(\d+\)/);
-                const protocolType = protocolTypeMatch ? protocolTypeMatch[1] : null;
-
-                const lengthMatch = line2.match(/length (\d+)/);
-                const length = lengthMatch ? lengthMatch[1] : null;
-
+        
+                // line1에서 정보 추출
+                const timestampMatch1 = line1.match(/(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+)/);
+                const timestamp1 = timestampMatch1 ? timestampMatch1[1] : null;
+        
+                // line2에서 정보 추출
+                const sourceIpMatch2 = line2.match(/(\d+\.\d+\.\d+\.\d+)\.(\d+) > (\d+\.\d+\.\d+\.\d+)\.(\d+)/);
+                const sourceIp2 = sourceIpMatch2 ? sourceIpMatch2[1] : null;
+                const sourcePort2 = sourceIpMatch2 ? sourceIpMatch2[2] : null;
+                const destinationIp2 = sourceIpMatch2 ? sourceIpMatch2[3] : null;
+                const destinationPort2 = sourceIpMatch2 ? sourceIpMatch2[4] : null;
+        
+                const protocolTypeMatch2 = line2.match(/proto (\w+) \(\d+\)/);
+                const protocolType2 = protocolTypeMatch2 ? protocolTypeMatch2[1] : null;
+        
+                const lengthMatch2 = line2.match(/length (\d+)/);
+                const length2 = lengthMatch2 ? lengthMatch2[1] : null;
+        
                 // 추출된 정보를 배열에 저장
-                const infoArray = [timestamp, sourceIp, sourcePort, destinationIp, destinationPort,  protocolType, `${length}`];
+                const infoArray = [timestamp1, sourceIp2, sourcePort2, destinationIp2, destinationPort2,  protocolType2, `${length2}`];
                 resultArray.push(infoArray);
             }
         }
