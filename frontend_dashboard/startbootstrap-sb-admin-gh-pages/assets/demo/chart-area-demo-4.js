73872d146f24f5acb6b3c4701e9ad6e86a3ee783
bsh8798 <beak8798@naver.com>
2023-11-15 01:56:01 +0900

    container4

---
+++ frontend_dashboard/startbootstrap-sb-admin-gh-pages/assets/demo/chart-area-demo-4.js
@@ -0,0 +1,81 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+function getArrayFromLocalStorage(key){
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : [];
+}
+function saveArrayToLocalStorage(key, array){
+  localStorage.setItem(key, JSON.stringify(array));
+}
+
+import {loadData, dataToExport} from '../../js/readPacket_4.js';
+
+function processData(data){
+  const myData = dataToExport;
+  const array = getArrayFromLocalStorage('myArray4') || [];
+  array .push(myData);
+  
+  var ctx = document.getElementById("myAreaChart4");
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ["time1", "time2", "time3", "time4", "time5", "time6", "time7", "time8", "time9", "time10"],
+      datasets: [{
+        label: "Sessions",
+        lineTension: 0.3,
+        backgroundColor: "rgba(2,117,216,0.2)",
+        borderColor: "rgba(2,117,216,1)",
+        pointRadius: 5,
+        pointBackgroundColor: "rgba(2,117,216,1)",
+        pointBorderColor: "rgba(255,255,255,0.8)",
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(2,117,216,1)",
+        pointHitRadius: 50,
+        pointBorderWidth: 2,
+        data: array,
+      }],
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          time: {
+            unit: 'date'
+          },
+          gridLines: {
+            display: false
+          },
+          ticks: {
+            maxTicksLimit: 10
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 500,
+            maxTicksLimit: 5
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, .125)",
+          }
+        }],
+      },
+      legend: {
+        display: false
+      }
+    }
+  });
+  if(array.length==10){
+    for(var i = 0; i<10; i++){
+      array.pop();
+    }
+  }
+  saveArrayToLocalStorage('myArray4s', array);
+
+}
+
+loadData((result)=>{
+  const myData = dataToExport;
+  processData(myData);
+});
---
+++ frontend_dashboard/startbootstrap-sb-admin-gh-pages/js/readPacket_4.js
@@ -0,0 +1,89 @@
+var xhr = new XMLHttpRequest();
+var filePath = 'tcpdump_test4.txt';
+var currentPage = 1;
+var rowsPerPage = 10;
+
+var dataToExport = 0;
+
+function loadData(callback){
+    xhr.open('GET', filePath, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var fileContent = xhr.responseText;
+            const lines = fileContent.split('\n');
+            const resultArray = [];
+    
+            for (let i = 0; i < lines.length; i += 2) {
+                if (i + 1 < lines.length) {
+                    const line1 = lines[i].trim();
+                    const line2 = lines[i + 1].trim();
+            
+                    // line1에서 정보 추출
+                    const timestampMatch1 = line1.match(/(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+)/);
+                    const timestamp1 = timestampMatch1 ? timestampMatch1[1] : null;
+    
+                    const sourceMacMatch1 = line1.match(/(\w+:\w+:\w+:\w+:\w+:\w+) > (\w+:\w+:\w+:\w+:\w+:\w+)/);
+                    const sourceMac1 = sourceMacMatch1 ? sourceMacMatch1[1] : null;
+                    const sourceMac2 = sourceMacMatch1 ? sourceMacMatch1[2] : null;
+             
+                    // line2에서 정보 추출
+                    const sourceIpMatch2 = line2.match(/(\d+\.\d+\.\d+\.\d+)\.(\d+) > (\d+\.\d+\.\d+\.\d+)\.(\d+)/);
+                    const sourceIp2 = sourceIpMatch2 ? sourceIpMatch2[1] : null;
+                    const sourcePort2 = sourceIpMatch2 ? sourceIpMatch2[2] : null;
+                    const destinationIp2 = sourceIpMatch2 ? sourceIpMatch2[3] : null;
+                    const destinationPort2 = sourceIpMatch2 ? sourceIpMatch2[4] : null;
+            
+                    const protocolTypeMatch2 = line1.match(/proto (\w+) \(\d+\)/);
+                    const protocolType2 = protocolTypeMatch2 ? protocolTypeMatch2[1] : null;
+            
+                    const lengthMatch2 = line1.match(/length (\d+)/);
+                    const length2 = lengthMatch2 ? lengthMatch2[1] : null;
+            
+                    // 추출된 정보를 배열에 저장
+                    const infoArray = [timestamp1, sourceIp2, sourcePort2, sourceMac1, destinationIp2, destinationPort2, sourceMac2,  protocolType2, `${length2}`];
+                    resultArray.push(infoArray);
+                }
+            }
+    
+            var tbody = document.querySelector("#datatablesSimple tbody");
+            var startIdx = (currentPage - 1)*rowsPerPage;
+            var endIdx = startIdx + rowsPerPage;
+    
+            tbody.innerHTML = "";
+            for (var i = startIdx; i < endIdx && i < resultArray.length; i++) {
+                var row = tbody.insertRow(-1);
+                row.setAttribute('data-index', i + 1);
+    
+                for (var j = 0; j < resultArray[i].length; j++) {
+                    var cell = row.insertCell(j);
+                    cell.textContent = resultArray[i][j];
+                }
+            }
+            dataToExport = resultArray.length;
+
+            if(callback){
+                callback(dataToExport);
+            }
+        }
+    };
+    
+    // 요청 전송
+    xhr.send();
+}
+
+function nextPage() {
+    currentPage++;
+    loadData();
+}
+function previousPage() {
+    if(currentPage > 1){
+        currentPage--;
+        loadData();
+    }
+}
+
+loadData();
+
+export {loadData, dataToExport};
+//export default dataToExport;
